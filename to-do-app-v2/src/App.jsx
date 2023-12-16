9e4c2573b35d49a063faa31b4b251cba1e69ddf7
ashleysimone1 <ashtucker1204@gmail.com>
2023-12-16 18:58:47 -0500

    fixed render bug and created basic file structure

---
+++ to-do-app-v2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+   <main>
+    <div>
+      <h1>To Do List</h1>
+    </div>
+   </main>
+  )
+}
+
+export default App
---
+++ to-do-app-v2/src/components/ToDoForm.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+
+
+function ToDoForm() {
+
+}
+
+export default ToDoForm
