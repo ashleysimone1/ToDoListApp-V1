33ce22973a0cdf0ca469a097bc4ed1862f44df12
ashleysimone1 <ashtucker1204@gmail.com>
2023-12-16 18:58:53 -0500

    created form submit handler syntax

--- to-do-app-v2/src/components/ToDoForm.jsx
+++ to-do-app-v2/src/components/ToDoForm.jsx
@@ -6,9 +6,10 @@ function ToDoForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-
-  }
+    const data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    e.target.reset()
+  };
 
     return (
       <div id = "user-todo-form" onSubmit={handleSubmit}>
@@ -17,7 +18,7 @@ function ToDoForm() {
           name="user-todo-input" 
           id="user-todo-input" 
           placeholder="What's next?"
-          value = { input }/>
+          action = { handleSubmit }/>
           <button type="submit" id='todo-submit-button'>Add Task</button>
         </form>
       </div>
